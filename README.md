316401bf62b70ce62ffc2660c8e94b72488c3233
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-01-08 18:08:37 +0500

    Migrate project to use JUCER Framework

--- README.md
+++
@@ -1,9 +0,0 @@
-VS Dependencies:
-Install Following using vcpkg:
-
-
-cd C:\vcpkg
-.\vcpkg install qt5:x64-windows
-
-For GUI:
-https://github.com/hanoglu/HComponentLibrary
---
+++ Source/Main.cpp
@@ -0,0 +1,86 @@
+// --------------------- Main Program -------------------------- /
+
+// --------------------- Includes ------------------------------
+#include <JuceHeader.h>
+#include "MainComponent.h"
+
+
+// --------------------- Main Application ----------------------
+
+class HostelManagementSystemApplication  : public juce::JUCEApplication
+{
+public:
+    HostelManagementSystemApplication() {}
+    const juce::String getApplicationName() override       { return ProjectInfo::projectName; }
+    const juce::String getApplicationVersion() override    { return ProjectInfo::versionString; }
+    bool moreThanOneInstanceAllowed() override             { return false; }
+
+    // Initialization Code
+    void initialise (const juce::String& commandLine) override
+    {
+        mainWindow.reset (new MainWindow (getApplicationName()));
+    }
+
+    // Application Shutdown Code
+    void shutdown() override
+    {
+        mainWindow = nullptr; // (deletes our window)
+    }
+
+    //==============================================================================
+    void systemRequestedQuit() override
+    {
+        quit();
+    }
+
+    void anotherInstanceStarted (const juce::String& commandLine) override
+    {
+    }
+
+    //==============================================================================
+    /*
+        This class implements the desktop window that contains an instance of
+        our MainComponent class.
+    */
+    class MainWindow : public juce::DocumentWindow
+    {
+    public:
+        MainWindow (juce::String name)
+            : DocumentWindow (name,
+                              juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                                          .findColour (juce::ResizableWindow::backgroundColourId),
+                              DocumentWindow::allButtons)
+        {
+            setUsingNativeTitleBar (false);
+            setContentOwned (new MainComponent(), true);
+
+           #if JUCE_IOS || JUCE_ANDROID
+            setFullScreen (true);
+           #else
+            setResizable (true, true);
+            setFullScreen(true);
+            centreWithSize (getWidth(), getHeight());
+           #endif
+
+            setVisible (true);
+        }
+
+        // This is called when the user tries to close this window. Here, we'll just
+        // ask the app to quit when this happens.
+        void closeButtonPressed() override
+        {
+            JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+// -------------------------------------------------------------
+// This macro generates the main() routine that launches the app.
+// -------------------------------------------------------------
+START_JUCE_APPLICATION (HostelManagementSystemApplication)
